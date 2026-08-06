1965fe6fbd4e7d5f513e8a5bd68cf037acaf6b69
Vipul Chandra Mishra <paritoshmishra5105@gmail.com>
2026-08-07 00:14:34 +0530

    add custom api res & err handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 // require('dotenv').config({path:'./env'})
 
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
+import connectDB from "./db/db.js";
 
 dotenv.config({
     path:'./.env'
 })
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port:`${process.env.PORT});
+        
+    })
+})
+.catch((error)=>{
+    console.log("MONGO DB connection failed",error)
+})
 
 
 
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,28 @@
+const asynchandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+ }
+
+export { asynchandler }
+
+/*
+ this is the illustration of how a higher order function is passed 
+const asynchandler = (func) => { } //simple arrow function
+const asynchandler = (func) => () => { } // higher order function passed as function
+const asynchandler=(func)=>{async()=>{}}
+
+    */
+
+// const asynchandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+//  }
+
+    
