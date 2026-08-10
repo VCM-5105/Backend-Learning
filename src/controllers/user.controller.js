c12433be8d62d0bb577c27f0c5d9314e5c9b52e6
Vipul Chandra Mishra <paritoshmishra5105@gmail.com>
2026-08-10 17:36:49 +0530

    usercontroller&user register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,68 @@
 import { asynchandler } from "../utils/asynchandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asynchandler(async (req,res) => {
-    res.status(200).json({
-        message:"First Postman use"
+    const { fullname, email, username, password } = req.body
+    console.log("email",email);
+    
+    // if (fullname === "") {
+    //     throw new ApiError(400," FullName is Required")
+    // } // this is okay for beginner to write multiple if block and check all the required if block message
+
+    // but better approach is
+    if (
+        [fullname,email,username,password].some((field)=>field?.trim()==="")
+    ) {
+        throw new ApiError(400,"All field are required")
+    }
+
+    const existedUser=User.findOne({
+        $or:[{username},{email}]
     })
+
+    if (existedUser) {
+        throw new ApiError(409,"User with email or username already exists")
+    }
+   
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+
+    const CoverImageLocalPath = req.files?.coverimage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverimage = await uploadOnCloudinary(CoverImageLocalPath)
+    
+    if (!avatar) {
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+   const user= await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverimage: coverimage.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+   })
+    
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!createdUser) {
+        throw new ApiError(500,"something went wrong while registering a user ")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 })
 
 export {registerUser}
